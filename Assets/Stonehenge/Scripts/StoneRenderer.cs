f321205f1e730b0d1c9d98dd4d8efeebf80305d2
Vitaly <kondvits@gmail.com>
2019-10-20 01:40:37 -0400

    Refactored, Perlin Noise COMPLETED

--- Assets/Stonehenge/Scripts/StoneRenderer.cs
+++ Assets/Stonehenge/Scripts/StoneRenderer.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections;
-using Unity.Collections;
 using UnityEngine;
 
 public class StoneRenderer : MonoBehaviour
@@ -28,176 +26,16 @@ public class StoneRenderer : MonoBehaviour
     private float width = 8; 
     private float radius = 2;
 
-
-
-    //TODO: solution create 6 game objects 
     void Start()
     {
-
-        //the resolutions are proportions of total points rendered with respect to the size
-        //int lineResolution = (int)(resolution * width / (width + height) / 2);
-        //int curveResolution = (int)(resolution * height / (width + height) / 2);
-
-        var topLine = DrawLine(resolution, AlignTopLine);
-        var bottomLine = DrawLine(resolution, AlignBottomLine);
-        var rightSide = DrawCurvedSide(resolution, AlignRightSide);
-        var leftSide = DrawCurvedSide(resolution, AlignLeftSide);
-
-        //AlignShape(topLine, bottomLine, rightSide, leftSide);
-
-        //Debug.Log(transform.position);
-        //Debug.Log(transform.rotation.eulerAngles);
-        //Debug.Log(transform.localScale);
+        DrawLine(resolution, AlignTopLine);
+        DrawLine(resolution, AlignBottomLine);
+        DrawCurvedSide(resolution, AlignRightSide);
+        DrawCurvedSide(resolution, AlignLeftSide);
 
     }
 
-
-    private void AlignShape(GameObject topLine, GameObject bottomLine, GameObject rightSide, GameObject leftSide)
-    {
-        //Start by aligning the top line
-        // then move to the sides
-        //lastly the bottom line
-
-        //top line is always higher than y = 0
-        //can use unity function to find the angle
-
-        //LineRenderer line = topLine.GetComponent<LineRenderer>();
-        //Debug.Log(line.transform.position);
-        //Debug.Log(topLine.transform.position);
-        //Vector3 firstPoint = line.GetPosition(0);
-        //Vector3[] pos = new Vector3[line.positionCount];
-        //line.GetPositions(pos);
-        //Vector3 lastPoint = line.GetPosition(line.positionCount - 1);
-        //Debug.Log(line.transform.TransformDirection(firstPoint));
-        //Debug.Log(line.transform.TransformDirection(lastPoint));
-
-        var lineRenderer = topLine.GetComponent<LineRenderer>();
-        var pos = lineRenderer.GetPosition(lineRenderer.positionCount - 1);
-        Debug.Log(topLine.transform.TransformPoint(pos));
-        Debug.Log(lineRenderer.transform.TransformPoint(pos));
-
-
-        //align the topLine
-        //AlignTopLine(topLine);
-        //AlignBottomLine(bottomLine);
-        //AlignRightSide(rightSide, bottomLine, topLine);
-
-        //AlignLeftSide(leftSide);
-
-
-
-        //TODO: just do half a circle and then scale it,
-
-
-
-
-    }
-
-    //private void AlignRightSide(GameObject rightSide, GameObject bottomLine, GameObject topLine)
-    //{
-    //    LineRenderer line = rightSide.GetComponent<LineRenderer>();
-    //    Vector3 firstPoint = line.GetPosition(0);
-    //    Vector3 lastPoint = line.GetPosition(line.positionCount - 1);
-
-
-
-    //    LineRenderer bottomLineRenderer = bottomLine.GetComponent<LineRenderer>();
-    //    Vector3 bottomLineLastPoint = bottomLineRenderer.GetPosition(bottomLineRenderer.positionCount - 1);
-
-    //    LineRenderer topLineRenderer = topLine.GetComponent<LineRenderer>();
-    //    Vector3 topLineLastPoint = topLineRenderer.GetPosition(topLineRenderer.positionCount - 1);
-
-    //    bottomLineLastPoint = bottomLine.transform.TransformPoint(bottomLineLastPoint);
-    //    topLineLastPoint = topLine.transform.TransformPoint(topLineLastPoint);
-
-    //    bottomLineLastPoint = transform.InverseTransformPoint(bottomLineLastPoint);
-    //    topLineLastPoint = transform.InverseTransformPoint(topLineLastPoint);
-
-
-    //    //firstPoint = rightSide.transform.TransformPoint(firstPoint);
-    //    //firstPoint = transform.InverseTransformPoint(firstPoint);
-
-    //    //lastPoint = rightSide.transform.TransformPoint(lastPoint);
-    //    //lastPoint = transform.InverseTransformPoint(lastPoint); // in parent coords
-
-    //    float scale = (topLineLastPoint.y - bottomLineLastPoint.y) / (firstPoint.x - lastPoint.x);
-
-    //    float angle = 90;
-    //    rightSide.transform.Rotate(0, 0, -angle);
-
-    //    //float scale = height / (firstPoint.x - lastPoint.x);
-    //    rightSide.transform.localScale = new Vector3(scale, 1, 1);
-
-    //    Debug.Log(bottomLineLastPoint);
-    //    bottomLineLastPoint = transform.TransformPoint(bottomLineLastPoint);
-
-    //    bottomLineLastPoint = rightSide.transform.InverseTransformPoint(bottomLineLastPoint);
-
-    //    Debug.Log(bottomLineLastPoint);
-    //    //Debug.Log(firstPoint);
-
-
-    //    //Vector3 scaledPos = new Vector3(transform.localScale.x * 0.5f * width, transform.localScale.y * -0.5f * height);
-    //    //Vector3 parentPosition = transform.TransformPoint(scaledPos);
-    //    //Vector3 firstPointLastPos = rightSide.transform.InverseTransformPoint(parentPosition);
-    //    rightSide.transform.position = bottomLineLastPoint;
-    //    //rightSide.transform.Translate(bottomLineLastPoint - firstPoint);
-
-    //}
-
-    private void AlignLeftSide(GameObject leftSide)
-    {
-        LineRenderer line = leftSide.GetComponent<LineRenderer>();
-        Vector3 firstPoint = line.GetPosition(0);
-        Vector3 lastPoint = line.GetPosition(line.positionCount - 1);
-
-        float scale = height / (firstPoint.x - lastPoint.x);
-        leftSide.transform.localScale = new Vector3(scale, 1, 1);
-
-        float angle = 90;
-        leftSide.transform.Rotate(0, 0, angle);
-
-        Vector3 scaledPos = new Vector3(transform.localScale.x * -0.5f * width, transform.localScale.y * 0.5f * height);
-        Vector3 parentPosition = transform.TransformPoint(scaledPos);
-        Vector3 firstPointLastPos = leftSide.transform.InverseTransformPoint(parentPosition);
-        leftSide.transform.Translate(firstPointLastPos - firstPoint);
-
-    }
-
-    //private void AlignTopLine(GameObject topLine)
-    //{
-    //    LineRenderer line = topLine.GetComponent<LineRenderer>();
-       
-    //    Vector3 firstPoint = line.GetPosition(0);
-    //    Vector3 lastPoint = line.GetPosition(line.positionCount - 1);
-
-    //    Vector3 connectedEndPoints = lastPoint - firstPoint;
-    //    topLine.transform.Rotate(0, 0, -Vector3.SignedAngle(Vector3.right, connectedEndPoints, Vector3.forward));
-
-    //    Vector3 scaledPos = new Vector3(transform.localScale.x * -width / 2, transform.localScale.y * height / 2);
-    //    Vector3 parentPosition = transform.TransformPoint(scaledPos);
-    //    Vector3 finalFirstPointPos = topLine.transform.InverseTransformPoint(parentPosition);
-    //    topLine.transform.Translate(finalFirstPointPos - firstPoint);
-
-    //}
-
-    //private void AlignBottomLine(GameObject bottomLine)
-    //{
-    //    LineRenderer line = bottomLine.GetComponent<LineRenderer>();
-    //    Vector3 firstPoint = line.GetPosition(0);
-    //    Vector3 lastPoint = line.GetPosition(line.positionCount - 1);
-
-    //    Vector3 connectedEndPoints = lastPoint - firstPoint;
-    //    bottomLine.transform.Rotate(0, 0, -Vector3.SignedAngle(Vector3.right, connectedEndPoints, Vector3.forward));
-
-    //    Vector3 scaledPos = new Vector3(transform.localScale.x * -width / 2, transform.localScale.y * -height / 2);
-    //    Vector3 parentPosition = transform.TransformPoint(scaledPos);
-    //    Vector3 finalFirstPointPos = bottomLine.transform.InverseTransformPoint(parentPosition);
-    //    bottomLine.transform.Translate(finalFirstPointPos - firstPoint);
-
-    //}
-
-    private GameObject DrawCurvedSide(int resolution, Action<Vector3[]> Align)
+    private void DrawCurvedSide(int resolution, Action<Vector3[]> Align)
     {
 
         GameObject curve = new GameObject("Curve");
@@ -237,11 +75,9 @@ public class StoneRenderer : MonoBehaviour
         Align(points);
 
         lineRenderer.SetPositions(points);
-
-        return curve;
     }
 
-    private GameObject DrawLine(int resolution, Action<Vector3[]> Align)
+    private void DrawLine(int resolution, Action<Vector3[]> Align)
     {
         GameObject line = new GameObject("Line");
         line.transform.SetParent(transform, false);
@@ -259,14 +95,6 @@ public class StoneRenderer : MonoBehaviour
 
         PerlinNoise noise = new PerlinNoise(initialAmplitude, amplitudeScalingFactor, initialFrequency, frequencyScalingFactor, numberOfOctaves);
 
-        //Vector3 offSet = new Vector3(-0.5f * width, 0.5f * height);
-
-        //float x = 0;
-        //float y = noise.GenerateNoise(x);
-        //points[0] = new Vector3(x, y) + offSet;
-
-
-
         for (int i = 0; i < pointCount; i++)
         {
             float x = i * width / (pointCount - 1);
@@ -277,11 +105,8 @@ public class StoneRenderer : MonoBehaviour
 
         Align(points);
 
-        lineRenderer.SetPositions(points); //transform is applied here localy
+        lineRenderer.SetPositions(points);
 
-
-
-        return line;
     }
 
     private void AlignTopLine(Vector3[] points)
@@ -290,22 +115,12 @@ public class StoneRenderer : MonoBehaviour
         Quaternion rotation = Quaternion.Euler(0, 0, -Vector3.SignedAngle(Vector3.right, connectedEndPoints, Vector3.forward));
         
         Vector3 translation = new Vector3(-0.5f * width, 0.5f * height) - points[0];
-        //Vector3 translation = new Vector3(-0.5f * width, 0.5f * height);
-        //Matrix4x4 m1 = Matrix4x4.Rotate(rotation);
-        //Matrix4x4 m2 = Matrix4x4.Translate(translation);//, rotation, Vector3.one);
-        Matrix4x4 m = Matrix4x4.TRS(translation, rotation, Vector3.one);//, rotation, Vector3.one);
+        Matrix4x4 m = Matrix4x4.TRS(translation, rotation, Vector3.one);
 
         for(int i = 0; i < points.Length; i++)
         {
             points[i] = m.MultiplyPoint3x4(points[i]);
-            //points[i] = m2.MultiplyPoint3x4(points[i]);
         }
-
-
-
-
-
-        //    topLine.transform.Rotate(0, 0, -Vector3.SignedAngle(Vector3.right, connectedEndPoints, Vector3.forward));
     }
 
     private void AlignBottomLine(Vector3[] points)
@@ -314,22 +129,12 @@ public class StoneRenderer : MonoBehaviour
         Quaternion rotation = Quaternion.Euler(0, 0, -Vector3.SignedAngle(Vector3.right, connectedEndPoints, Vector3.forward));
 
         Vector3 translation = new Vector3(-0.5f * width, -0.5f * height) - points[0];
-        //Vector3 translation = new Vector3(-0.5f * width, -0.5f * height);
-        //Matrix4x4 m1 = Matrix4x4.Rotate(rotation);
-        //Matrix4x4 m2 = Matrix4x4.Translate(translation);//, rotation, Vector3.one);
-        Matrix4x4 m = Matrix4x4.TRS(translation, rotation, Vector3.one);//, rotation, Vector3.one);
+        Matrix4x4 m = Matrix4x4.TRS(translation, rotation, Vector3.one);
 
         for (int i = 0; i < points.Length; i++)
         {
             points[i] = m.MultiplyPoint3x4(points[i]);
-            //points[i] = m2.MultiplyPoint3x4(points[i]);
         }
-
-
-
-
-
-        //    topLine.transform.Rotate(0, 0, -Vector3.SignedAngle(Vector3.right, connectedEndPoints, Vector3.forward));
     }
 
     private void AlignRightSide(Vector3[] points)
@@ -346,14 +151,11 @@ public class StoneRenderer : MonoBehaviour
         Matrix4x4 m3 = Matrix4x4.Translate(translation);
         points[0] = m3.MultiplyPoint3x4(points[0]);
 
-        //Matrix4x4 m4 = Matrix4x4.TRS(translation, rotation, new Vector3(1, scale, 1));
-
         for (int i = 1; i < points.Length; i++)
         {
             points[i] = m1.MultiplyPoint3x4(points[i]);
             points[i] = m2.MultiplyPoint3x4(points[i]);
             points[i] = m3.MultiplyPoint3x4(points[i]);
-            //points[i] = m2.MultiplyPoint3x4(points[i]);
         }
 
     }
@@ -372,14 +174,11 @@ public class StoneRenderer : MonoBehaviour
         Matrix4x4 m3 = Matrix4x4.Translate(translation);
         points[0] = m3.MultiplyPoint3x4(points[0]);
 
-        //Matrix4x4 m4 = Matrix4x4.TRS(translation, rotation, new Vector3(1, scale, 1));
-
         for (int i = 1; i < points.Length; i++)
         {
             points[i] = m1.MultiplyPoint3x4(points[i]);
             points[i] = m2.MultiplyPoint3x4(points[i]);
             points[i] = m3.MultiplyPoint3x4(points[i]);
-            //points[i] = m2.MultiplyPoint3x4(points[i]);
         }
 
     }
